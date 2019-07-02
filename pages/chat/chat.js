b77c10a662e0d2c6022851e77899eb7fd44e952a
易林军 <1441457008@qq.com>
2019-07-03 01:15:15 +0800

    add

---
+++ pages/chat/chat.js
@@ -0,0 +1,13 @@
+Page({
+  data: {
+    background: ['demo-text-1', 'demo-text-2'],
+    indicatorDots: false,
+    vertical: false,
+    autoplay: false,
+    circular: false,
+    interval: 2000,
+    duration: 500,
+    previousMargin: 0,
+    nextMargin: 0
+  }
+})
--- pages/chatlist/chatlist.js
+++ pages/chatlist/chatlist.js
@@ -15,6 +15,16 @@ Page({
     isChatList: true,
     showModal: false,
     tempHeadImgPath: "",
+
+    background: ['demo-text-1', 'demo-text-2', 'demo-text-3'],
+    indicatorDots: true,
+    vertical: false,
+    autoplay: false,
+    circular: false,
+    interval: 2000,
+    duration: 500,
+    previousMargin: 0,
+    nextMargin: 0
   },
 
 
