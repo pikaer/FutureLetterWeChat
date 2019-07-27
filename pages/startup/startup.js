428a345190513fc38e5c5c91aba38570045a2ce8
易林军 <1441457008@qq.com>
2019-07-27 13:08:59 +0800

    完善页面

--- pages/startup/startup.js
+++ pages/startup/startup.js
@@ -59,7 +59,7 @@ Page({
       //用户按了拒绝按钮
       wx.showModal({
         title: '警告',
-        content: '您点击了拒绝授权，将无法进入小程序，请授权之后再进入!!!',
+        content: '获取用户信息失败，需要授权才能继续使用',
         showCancel: false,
         confirmText: '返回授权',
         success: function(res) {
